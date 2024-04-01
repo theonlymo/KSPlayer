1ef4c5bd883d75b16de4ac09f8d7c073f832c434
TheOnlyMo <145556517+theonlymo@users.noreply.github.com>
2024-04-01 04:19:56 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -65,6 +65,6 @@ if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift") {
     ]
 } else {
     package.dependencies += [
-        .package(url: "https://github.com/kingslay/FFmpegKit.git", from: "main")
+        .package(url: "https://github.com/kingslay/FFmpegKit.git", branch: "main")
     ]
 }
